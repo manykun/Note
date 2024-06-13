0d7d34930543dc7bf028ea3ce85ee5e626cddc49
manykun <manykun@163.com>
2024-06-13 23:27:33 +0800

    feat：粗糙实现笔记列表

--- frontend/app/src/main/java/android/example/note/NoteListFragment.java
+++ frontend/app/src/main/java/android/example/note/NoteListFragment.java
@@ -2,34 +2,69 @@ package android.example.note;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.xuexiang.xormlite.InternalDataBaseRepository;
+import com.xuexiang.xormlite.db.DBService;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class NoteListFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private NoteAdapter noteAdapter;
+    private List<NoteModel> noteList;
+    private DBService<NoteModel> mDBService;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDBService = InternalDataBaseRepository.getInstance().getDataBase(NoteModel.class);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.notelistfragment, container, false);
 
-        Button button = view.findViewById(R.id.add_note_button);
-        // 在第一个Fragment中创建一个Bundle对象并设置数据
-        Bundle bundle = new Bundle();
-        bundle.putInt("Note_id", Integer.parseInt("1"));
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
 
-        Fragment noteaddFragment = new NoteAddFragment();
-        noteaddFragment.setArguments(bundle);
+        try {
+            noteList = mDBService.queryAll();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
-        button.setOnClickListener(v -> {
-             assert getFragmentManager() != null;
-             getFragmentManager().beginTransaction()
-                     .replace(R.id.fragment_container, noteaddFragment)
-                     .addToBackStack(null)
-                     .commit();
+        noteAdapter = new NoteAdapter(noteList);
+        recyclerView.setAdapter(noteAdapter);
 
+        Toolbar toolbar = view.findViewById(R.id.notelist_toolbar);
+        toolbar.inflateMenu(R.menu.note_list);
 
-        });
+//        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+//            @Override
+//            public boolean onMenuItemClick(MenuItem item) {
+//                if (item.getItemId() == R.id.action_photo) {
+//                    showImageSourceDialog();
+//                    return true;
+//                } else if (item.getItemId() == R.id.action_record) {
+//                    showRecordSourceDialog();
+//                    return true;
+//                } else {
+//                    return false;
+//                }
+//            }
+//        });
+
 
         return view;
 
